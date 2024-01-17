fd61305476f7feb50b0537f17819f4b34c326ce4
Aman Grover <60424294+amang234@users.noreply.github.com>
2024-01-17 23:26:04 +0530

    Time: 2 ms (65.84%), Space: 8.7 MB (41.08%) - LeetHub

---
+++ 1319-unique-number-of-occurrences/1319-unique-number-of-occurrences.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool uniqueOccurrences(vector<int>& arr) {
+        unordered_map<int,int>mpp;
+        unordered_map<int,int>occur;
+
+        for(auto i :  arr){
+            mpp[i]++;
+        }
+        for(auto it : mpp){
+            if(occur[it.second]) return false;
+            occur[it.second]++;
+        }
+
+        return true;
+    }
+};
