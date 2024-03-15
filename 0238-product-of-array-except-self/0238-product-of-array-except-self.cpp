0c3289b80c5109d298f57041a786d73a9e568ed5
Aman Grover <60424294+amang234@users.noreply.github.com>
2024-03-15 09:38:08 +0530

    Time: 25 ms (6.72%), Space: 27.4 MB (23.95%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
@@ -1,21 +1,15 @@
 class Solution {
 public:
     vector<int> productExceptSelf(vector<int>& nums) {
-        int n = nums.size();
-        vector<int> left(n);
-        vector<int> right(n);
-        vector<int> ans(n);
-        left[0] =1;
-        right[n-1] =1;
-        for(int i = 1 ; i < n ; i++){
-            left[i] = left[i-1]*nums[i-1];
-        }
-        for(int i = n-2 ; i >= 0; i--){
-            right[i] = right[i+1]*nums[i+1];
-        }
-        for(int i=0; i< n; i++){
-            ans[i] = left[i]*right[i];
-        }
-        return ans;
+        int n= nums.size();
+
+        vector<int> prefix(n,1);
+        vector<int> suffix(n,1);
+        for(int i = 1;i<n;i++) prefix[i] = prefix[i-1] * nums[i-1];
+        for(int i = n-2;i>=0;--i) suffix[i] = suffix[i+1] * nums[i+1];
+
+        vector<int> answer(n);
+        for(int i = 0; i<n ; ++i) answer[i] = prefix[i] * suffix[i];
+        return answer;
     }
 };
