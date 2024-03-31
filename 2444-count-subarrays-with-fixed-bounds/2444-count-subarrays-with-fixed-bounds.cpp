12e2509baa3ff1df2673ab2e868aeb4637755232
Aman Grover <60424294+amang234@users.noreply.github.com>
2024-03-31 14:39:14 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 2444-count-subarrays-with-fixed-bounds/2444-count-subarrays-with-fixed-bounds.cpp
@@ -0,0 +1,9 @@
+class Solution {
+public:
+    long long countSubarrays(vector<int>& nums, int minK, int maxK) {
+        long long res =0;
+        int bad_idx = -1;
+        int left_idx = -1;
+        int right_idx = -1;
+    }
+};
