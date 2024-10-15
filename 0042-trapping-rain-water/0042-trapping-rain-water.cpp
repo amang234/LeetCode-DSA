a68d66971b75c722bd6525257d2389faf286bd21
Aman Grover <60424294+amang234@users.noreply.github.com>
2024-10-16 00:17:11 +0530

    Time: 16 ms (31.91%), Space: 24.9 MB (50.21%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.cpp
+++ 0042-trapping-rain-water/0042-trapping-rain-water.cpp
@@ -1,19 +1,13 @@
 class Solution {
 public:
-    int trap(std::vector<int>& height) {
-        int i = 0, left_max = height[0], sum = 0;
-        int j = height.size() - 1, right_max = height[j];
-        while (i < j) {
-            if (left_max <= right_max) {
-                sum += (left_max - height[i]);
-                i++;
-                left_max = std::max(left_max, height[i]);
-            } else {
-                sum += (right_max - height[j]);
-                j--;
-                right_max = std::max(right_max, height[j]);
-            }
+    //total water is trapped into the bars
+    int trap(vector<int>& h) {
+        int l=0,r=h.size()-1,lmax=INT_MIN,rmax=INT_MIN,ans=0;
+        while(l<r){
+            lmax=max(lmax,h[l]);
+            rmax=max(rmax,h[r]);
+            ans+=(lmax<rmax)?lmax-h[l++]:rmax-h[r--];
         }
-        return sum;
+        return ans;
     }
 };
