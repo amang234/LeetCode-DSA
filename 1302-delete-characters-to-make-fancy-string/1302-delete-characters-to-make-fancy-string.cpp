c9da239b162b5b84e3330660eba46cd01e92af48
Aman Grover <60424294+amang234@users.noreply.github.com>
2025-07-21 22:04:02 +0530

    Time: 31 ms (46.51%), Space: 44.7 MB (13.57%) - LeetHub

---
+++ 1302-delete-characters-to-make-fancy-string/1302-delete-characters-to-make-fancy-string.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    string makeFancyString(string s) {
+        string result;
+        for (char c : s) {
+            if (result.size() >= 2 && result.back() == c && result[result.size() - 2] == c) {
+                continue;
+            }
+            result.push_back(c);
+        }
+        return result;
+    }
+};
