84ba2feb9b01fee32730b3bc12e337564d58e693
Aman Grover <60424294+amang234@users.noreply.github.com>
2025-09-23 20:48:23 +0530

    Update 0057-insert-interval.cpp

--- 0057-insert-interval/0057-insert-interval.cpp
+++ 0057-insert-interval/0057-insert-interval.cpp
@@ -4,7 +4,7 @@ public:
         vector<vector<int>> merged;
         int i = 0;
 
-        while(i<intervals.size() && intervals[i][1] < newInterval[1]) {
+        while(i<intervals.size() && intervals[i][1] < newInterval[0]) {
             merged.push_back(intervals[i]);
             i++;
         }
